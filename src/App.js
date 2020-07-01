be85d7dffc3284cf80841310e85c68b1225adb45
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 14:17:08 -0700

    app file created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+export default class App extends React.Component {
+    state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  
+    handleClick = buttonName => {
+      this.setState(calculate(this.state, buttonName));
+    };
