8257da027cf6b26dad5b37659bb7570fda58551a
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 13:28:00 -0700

    get the division total

--- src/calculator.js
+++ src/calculator.js
@@ -33,3 +33,37 @@ export default function calculate(obj, buttonName) {
           }
           return { next: buttonName };
         }
+        // If there is no operation, update next and clear the value
+    if (obj.next) {
+        const next = obj.next === "0" ? buttonName : obj.next + buttonName;
+        return {
+          next,
+          total: null,
+        };
+      }
+      return {
+        next: buttonName,
+        total: null,
+      };
+    }
+
+    if (buttonName === "%") {
+        if (obj.operation && obj.next) {
+          const result = operate(obj.total, obj.next, obj.operation);
+          return {
+            total: Big(result)
+              .div(Big("100"))
+              .toString(),
+            next: null,
+            operation: null,
+          };
+        }
+        if (obj.next) {
+          return {
+            next: Big(obj.next)
+              .div(Big("100"))
+              .toString(),
+          };
+        }
+        return {};
+      }
