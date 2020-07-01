c02ae3a1c75cf8693e7040fe48dc168883e0ffe5
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 14:48:20 -0700

    imports

--- src/ButtonPanel.js
+++ src/ButtonPanel.js
@@ -1,4 +1,8 @@
+import Button from "./Button";
+import React from "react";
+import PropTypes from "prop-types";
 
+import "./ButtonPanel.css";
 
 export default class ButtonPanel extends React.Component {
     static propTypes = {
