9378924000d87f6e42419493d2f9ced03c25bc94
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 13:40:55 -0700

    save oeration and shift to total

--- src/calculator.js
+++ src/calculator.js
@@ -119,3 +119,18 @@ export default function calculate(obj, buttonName) {
     };
   }
 
+  // no operation yet, but the user typed one
+
+  // The user hasn't typed a number yet, just save the operation
+  if (!obj.next) {
+    return { operation: buttonName };
+  }
+
+  // save the operation and shift 'next' into 'total'
+  return {
+    total: obj.next,
+    next: null,
+    operation: buttonName,
+  };
+}
+
