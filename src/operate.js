7289303a90c0d7a18f19db53460dd7d52cfeda9f
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 14:12:33 -0700

    operate minus

---
+++ src/operate.js
@@ -0,0 +1,9 @@
+export default function operate(numberOne, numberTwo, operation) {
+    const one = Big(numberOne || "0");
+    const two = Big(numberTwo || (operation === "÷" || operation === 'x' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
+    if (operation === "+") {
+      return one.plus(two).toString();
+    }
+    if (operation === "-") {
+        return one.minus(two).toString();
+      }
