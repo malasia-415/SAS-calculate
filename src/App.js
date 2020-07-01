15e3e07332607ae47a8e484b2237f905b46716d9
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 14:18:40 -0700

    handleClickk button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
 
 export default class App extends React.Component {
     state = {
@@ -10,3 +14,14 @@ export default class App extends React.Component {
     handleClick = buttonName => {
       this.setState(calculate(this.state, buttonName));
     };
+
+    render() {
+        return (
+          <div className="component-app">
+            <Display value={this.state.next || this.state.total || "0"} />
+            <ButtonPanel clickHandler={this.handleClick} />
+          </div>
+        );
+      }
+    }
+    
