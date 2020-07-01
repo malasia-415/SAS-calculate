77fe7fa1ab17240cd43be6874cafba845331e91c
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 14:50:40 -0700

    dispaly content

---
+++ src/Display.js
@@ -0,0 +1,15 @@
+
+
+export default class Display extends React.Component {
+    static propTypes = {
+      value: PropTypes.string,
+    };
+  
+    render() {
+      return (
+        <div className="component-display">
+          <div>{this.props.value}</div>
+        </div>
+      );
+    }
+  }
